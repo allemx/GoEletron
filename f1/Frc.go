6067060d8bc24cae403a330fd194d335216dc131
allemx <72419172+allemx@users.noreply.github.com>
2022-09-16 20:40:24 -0300

    Update Frc.go

--- f1/Frc.go
+++ f1/Frc.go
@@ -1,13 +1,19 @@
-//Calculo filtro passivo RC Frequencia 
-//de corte Resistor em Ohms Capacitor em 
-//farads
-package main import "fmt" func main() { 
-	r1 := 2654.0 // 2.654 K ohms c1 
-	:= 1e-06 // 1 microfarad
-	fc := Frequencia(r1, c1) 
-	fmt.Printf("%0.1f Hertz", fc)
+// Exemplo de calculo filtro passivo RC
+// Frequencia de corte
+// Resistor em Ohms e Capacitor em farads
+// Resultado: 60 hertz
+
+package main
+
+import "fmt"
+
+func main() {
+	r1 := 2654.0 // 2.654 K ohms
+	c1 := 1e-06  // 1 microfarad
+	fc := Frequencia(r1, c1)
+	fmt.Printf("%0.1f hertz", fc)
 }
-func Frequencia(r, c float64) float64 { 
-	f := 1 / (2 * 3.14 * r * c) 
+func Frequencia(r, c float64) float64 {
+	f := 1 / (2 * 3.14 * r * c)
 	return f
 }
