3cb95a19347cf2ef9334aa6e967325ed537933c2
allemx <72419172+allemx@users.noreply.github.com>
2022-09-16 22:54:47 -0300

    Update Flc.go

--- f2/Flc.go
+++ f2/Flc.go
@@ -1,19 +1,22 @@
-//Função calcula freq ressonancia de um 
+//Função calcula freq ressonancia de um
 //circuito LC
 // Resultado:5.033069648525193 Mhz
 
 package main
 
- import ( "fmt" "math" ) 
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	l1 := 100e-06 // 100 microhenrys 
-	c1 := 10e-12 // 10 pf
-        res :=	Ressonancia(l1, c1) 
+	l1 := 100e-06 // 100 microhenrys
+	c1 := 10e-12  // 10 pf
+	res := Ressonancia(l1, c1)
 	fmt.Println(res/1000000, "Mhz")
 }
-func Ressonancia(l, c float64) float64 { 
+func Ressonancia(l, c float64) float64 {
 	rq := math.Sqrt(l * c)
- f := 1 /(2 * 3.1415 * rq)
-         return f
+	f := 1 / (2 * 3.1415 * rq)
+	return f
 }
