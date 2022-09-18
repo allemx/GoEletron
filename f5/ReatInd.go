ce6456cf2852eb374f9f75b04a0d9c94f395cbdb
allemx <allemx@gmail.com>
2022-09-18 17:23:09 -0300

    Func Calc Reatância Indutiva

---
+++ f5/ReatInd.go
@@ -0,0 +1,21 @@
+// Reatância Indutiva
+// Resultado: 376.99 
+// ohms 
+// https://go.dev/play/p/w5L36DMip9x
+
+package main 
+
+import ( "fmt"
+        "math" ) 
+
+func main() {
+	fr1 := 60.0 // 60 hertz 
+         l1 :=	1.0 // 1 henry
+         Xl :=ReatIndut(fr1, l1) 
+	 fmt.Printf("%.02f Ohms", Xl)
+}
+func ReatIndut(f, l float64) float64 { 
+	pi := math.Pi
+        xl := 2 * pi * f * l
+         return xl
+}
