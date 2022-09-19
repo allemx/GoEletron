73e8e1f738f2a53def481462ec1792c5010ae308
allemx <allemx@gmail.com>
2022-09-19 16:54:36 -0300

    Func calc impedância RL paralelo

---
+++ f10/ZRLpar.go
@@ -0,0 +1,15 @@
+// Calc impedância RL paralelo 
+// Resultado: 39.826 ohms 
+// https://go.dev/play/p/3OwE8YjfFQu
+
+package main import ( "fmt" "math" ) 
+
+func main() {
+	r1 := 47.0 l1 := 75.0 
+        Z1 := ZRLpar(r1, l1) 
+         fmt.Printf("%.3f ohms", Z1)
+}
+func ZRLpar(r, l float64) float64 {
+ Z := r * l / (math.Sqrt(r*r + l*l)) 
+	return Z
+}
