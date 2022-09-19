02d78b09ea4148a042042482fe15d93a090268fa
allemx <allemx@gmail.com>
2022-09-19 17:45:10 -0300

    Func calc impedância RC paralelo

---
+++ f11/ZRCpar.go
@@ -0,0 +1,17 @@
+// Calc impedância RC paralelo 
+// Resultado: 39.826 
+// https://go.dev/play/p/Yi65vxcgE1-
+
+package main import ( "fmt"
+                      "math" ) 
+
+func main() {
+	r1 := 47.0 
+        c1 := 75.0 //reat capac 
+        Z1 := ZRCpar(r1, c1) 
+	   fmt.Printf("%.3f ohms", Z1)
+}
+func ZRCpar(r, c float64) float64 { 
+        Z := r * c / (math.Sqrt(r*r + c*c)) 
+	   return Z
+}
